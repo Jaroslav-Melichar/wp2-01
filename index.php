aca0bfd0a9d6e732fa5dd5fdd2baedddbbf73586
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-09-13 11:36:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Převod peněz</h1>
+
+<form action="index.php" method="post" > 
+
+Částka :
+<input type="number" value> <br>
+
+<h2> Převod : </h2> 
+<input type="radio" id="eur_czk" name="prachy" value="eur_czk">
+<label for="eur_czk">EUR na CZK</label><br>
+<input type="radio" id="czk_eur" name="prachy" value="czk_eur">
+<label for="czk_eur">CZK na EUR</label>
+<br>
+<br>
+<input type="submit" value="Převést" name="submit" >
+</form>
+
+</body>
+</html>
